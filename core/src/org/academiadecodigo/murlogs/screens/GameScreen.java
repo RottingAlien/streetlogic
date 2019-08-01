ac98d3e789519bd046d468c0203b3056f1725a4a
Alex <cradleofcorpses@gmail.com>
2019-08-01 18:31:55 +0100

    First commit with libgdx setup

---
+++ core/src/org/academiadecodigo/murlogs/screens/GameScreen.java
@@ -0,0 +1,57 @@
+package org.academiadecodigo.murlogs.screens;
+
+import com.badlogic.gdx.Gdx;
+import com.badlogic.gdx.Screen;
+import com.badlogic.gdx.graphics.GL20;
+import com.badlogic.gdx.graphics.Texture;
+import org.academiadecodigo.murlogs.StreetLogic;
+
+public class GameScreen implements Screen {
+
+    StreetLogic game;
+    Texture img;
+
+    public GameScreen(StreetLogic game){
+        this.game=game;
+        img = new Texture("badlogic.jpg");
+    }
+
+    @Override
+    public void show() {
+
+    }
+
+    @Override
+    public void render(float delta) {
+        Gdx.gl.glClearColor(1, 0, 0, 1);
+        Gdx.gl.glClear(GL20.GL_COLOR_BUFFER_BIT);
+        game.batch.begin();
+        game.batch.draw(img, 0, 0);
+        game.batch.end();
+    }
+
+    @Override
+    public void resize(int width, int height) {
+
+    }
+
+    @Override
+    public void pause() {
+
+    }
+
+    @Override
+    public void resume() {
+
+    }
+
+    @Override
+    public void hide() {
+
+    }
+
+    @Override
+    public void dispose() {
+        img.dispose();
+    }
+}
