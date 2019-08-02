86958d6784890e6cd5c90a3ce18d71ad21082610
JosePalinhos <josepalinhos@gmail.com>
2019-08-02 10:50:19 +0100

    added menus

---
+++ core/src/org/academiadecodigo/murlogs/Initial.java
@@ -0,0 +1,11 @@
+package org.academiadecodigo.murlogs;
+
+import com.badlogic.gdx.graphics.Texture;
+import com.badlogic.gdx.graphics.g2d.Sprite;
+
+public class Initial {
+
+    public Texture initialMenuTexture = new Texture("initialMenu.jpg");
+    public Sprite sprite = new Sprite(initialMenuTexture);
+
+}
---
+++ core/src/org/academiadecodigo/murlogs/screens/WinnerMenu.java
@@ -0,0 +1,9 @@
+package org.academiadecodigo.murlogs.screens;
+
+import com.badlogic.gdx.graphics.Texture;
+import com.badlogic.gdx.graphics.g2d.Sprite;
+
+public class WinnerMenu {
+    public Texture winnerMenuTexture = new Texture("winnerMenu.jpg");
+    public Sprite sprite = new Sprite(winnerMenuTexture);
+}
